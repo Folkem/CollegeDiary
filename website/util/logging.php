cac3bbf0de305db843086990d9b25ab9666ced63
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-21 17:15:15 +0200

    add logging object
    Add website/util/logging.php file, which contains ready-to-use logging
    object. It logs to website/logs/all.log, info.log, debug.log and error.log
    all the logs, which corresponds with their names (info.log contains only
    INFO and NOTICE messages, all.log contains all 8 levels of logs etc.).
    It's a more easier way, than making separate logging object for each
    file/page, where the only need is to include the logging file.

---
+++ website/util/logging.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once "loader.php";
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+$logger = new Logger('general');
+$logger->pushHandler(new StreamHandler(__DIR__."/logs/debug.log", Logger::DEBUG));
+$logger->pushHandler(new StreamHandler(__DIR__."/logs/info.log", Logger::INFO, false));
+$logger->pushHandler(new StreamHandler(__DIR__."/logs/error.log", Logger::WARNING, false));
+$logger->pushHandler(new StreamHandler(__DIR__ . "/logs/all.log", Logger::DEBUG));
