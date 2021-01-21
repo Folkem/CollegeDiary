b8f601cc2ca61ad180e106a3e57a26b2c58c4400
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-21 17:21:55 +0200

    add composer dependencies autoloading

--- website/util/loader.php
+++ website/util/loader.php
@@ -1,8 +1,9 @@
 <?php
 
+require_once "../vendor/autoload.php";
+
 function loadClass($className) {
     require $_SERVER['DOCUMENT_ROOT'] . "/classes/$className.php";
 }
 
 spl_autoload_register('loadClass');
-?>
