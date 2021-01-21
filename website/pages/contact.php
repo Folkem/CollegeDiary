2fa10e017c9ebfed1821e419c67f07ac78536cf1
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-21 17:09:09 +0200

    add missing style references

--- website/pages/contact.php
+++ website/pages/contact.php
@@ -9,7 +9,8 @@ require_once "../util/auth_check.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Зворотній зв'язок</title>
+    <title>Зворотній зв'язок — Онлайн-щоденник</title>
+    <link rel="stylesheet" href="../styles/font-awesome/all.min.css">
     <link rel="stylesheet" href="../styles/normalize.css">
     <link rel="stylesheet" href="../styles/reset.css">
     <link rel="stylesheet" href="../styles/sections.css">
