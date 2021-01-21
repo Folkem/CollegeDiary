4357fdf76cec311c8495f6749b3777ee36f3facd
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-21 17:07:35 +0200

    improve news page

--- website/pages/news.php
+++ website/pages/news.php
@@ -9,33 +9,40 @@ require_once "../util/auth_check.php";
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Новини</title>
+    <title>Новини — Онлайн-щоденник</title>
+    <link rel="stylesheet" href="../styles/font-awesome/all.min.css">
     <link rel="stylesheet" href="../styles/normalize.css">
     <link rel="stylesheet" href="../styles/reset.css">
     <link rel="stylesheet" href="../styles/sections.css">
     <link rel="stylesheet" href="../styles/general.css">
+    <link rel="stylesheet" href="../styles/news.css">
     <script src="../scripts/sections.js"></script>
 </head>
 <body>
 <?php require_once "../sections/header.php"; ?>
 
-<?php
-StorageRepository::load();
+<main class="main">
+    <?php
+    // TODO: make news system look cute and effective
+    StorageRepository::load();
 
-$newsArray = StorageRepository::getNews();
+    $newsArray = StorageRepository::getNews();
 
-foreach ($newsArray as $item) {?>
-    <div>
-        <h1><?= $item->getHeader(); ?></h1>
-        <p>
-            <?= $item->getText(); ?>
-        </p>
-        <div>
-            <?= $item->getDate()->format('j.m.Y'); ?>
+    foreach ($newsArray as $item) { ?>
+        <div class="news-item">
+            <a class="link" href="/pages/news_item.php?item=<?= $item->getId() ?>">
+                <h1><?= $item->getHeader(); ?></h1>
+                <p>
+                    <?= $item->getText(); ?>
+                </p>
+                <div>
+                    <?= $item->getDate()->format('j.m.Y'); ?>
+                </div>
+            </a>
         </div>
-    </div>
-<?php }
-?>
+    <?php }
+    ?>
+</main>
 
 <?php require_once "../sections/footer.php"; ?>
 </body>
