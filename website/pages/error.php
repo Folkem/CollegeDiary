ad40b19125e7dcfd668f60643c51c09651eb712e
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-21 17:23:03 +0200

    add error page

---
+++ website/pages/error.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/normalize.css">
+    <link rel="stylesheet" href="../styles/sections.css">
+    <link rel="stylesheet" href="../styles/general.css">
+    <link rel="stylesheet" href="../styles/error.css">
+    <title>Помилка</title>
+</head>
+<body>
+<?php require_once "../sections/header.php"; ?>
+<main class="error-main">
+    <h1 class="error-message">Запрошена вами сторінка не була знайдена</h1>
+</main>
+<?php require_once "../sections/footer.php"; ?>
+</body>
+</html>
