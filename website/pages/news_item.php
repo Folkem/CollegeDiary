7e5c2a3cead261cbdb2afe14fdad78ce21e25187
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-21 17:09:17 +0200

    add news_item page
    Add news_item page with news-item.css stylesheet.

---
+++ website/pages/news_item.php
@@ -0,0 +1,67 @@
+<?php
+require_once "../util/loader.php";
+require_once "../util/auth_check.php";
+
+$item = null;
+$redirect = true;
+
+if (isset($_GET['item'])) {
+    try {
+        $itemId = intval($_GET['item']);
+
+        StorageRepository::load();
+        $newsArray = StorageRepository::getNews();
+
+        $filteredNews = array_filter($newsArray, function($newsItem) use($itemId) {
+            return ($newsItem->getId() === $itemId);
+        });
+
+        // todo: logging
+
+        if (count($filteredNews) > 1) {
+            // TODO: log error
+        } else if (count($filteredNews) == 0) {
+            $redirect = true;
+        } else {
+            $item = $filteredNews[0];
+            $redirect = false;
+        }
+    } catch (Exception $exception) {
+        $redirect = true;
+    }
+}
+
+if ($redirect) {
+    header("Location: /pages/news.php");
+}
+?>
+<!doctype html>
+<html lang="uk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?= $item->getHeader() ?> — Онлайн-щоденник</title>
+    <link rel="stylesheet" href="../styles/normalize.css">
+    <link rel="stylesheet" href="../styles/reset.css">
+    <link rel="stylesheet" href="../styles/sections.css">
+    <link rel="stylesheet" href="../styles/general.css">
+    <link rel="stylesheet" href="../styles/news-item.css">
+    <script src="../scripts/sections.js"></script>
+</head>
+<body>
+<?php require_once "../sections/header.php"; ?>
+
+<main class="main news-item">
+    <h1 class="news-item__header"><?= $item->getHeader() ?></h1>
+    <p class="news-item__date">
+        <?= $item->getDate()->format('Y/m/d — H:i:s') ?>
+    </p>
+    <p class="news-item__text">
+        <?= $item->getText() ?>
+    </p>
+</main>
+
+<?php require_once "../sections/footer.php"; ?>
+</body>
