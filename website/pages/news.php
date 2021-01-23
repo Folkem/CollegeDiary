1eac5cdb7814cac5e512556c87e3f0896c11e16e
Maks Lisitsyn <maxxfoxx2012@gmail.com>
2021-01-23 21:45:58 +0200

    add news filtering
    Add news real-time filtering by:
    * name (selects all news, which header case-insentively contains
    name input);
    * publishing date (start and end includingly);
    * included keywords (selects the news, which contains at least one of
    the requested keywords; separated by space; made with <div
    contenteditable="true"></div> so to improve in in future to those inputs)

--- website/pages/news.php
+++ website/pages/news.php
@@ -10,19 +10,20 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/util/auth_check.php";
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Новини — Онлайн-щоденник</title>
-    <link rel="stylesheet" href="../styles/font-awesome/all.min.css">
-    <link rel="stylesheet" href="../styles/normalize.css">
-    <link rel="stylesheet" href="../styles/reset.css">
-    <link rel="stylesheet" href="../styles/sections.css">
-    <link rel="stylesheet" href="../styles/general.css">
-    <link rel="stylesheet" href="../styles/news.css">
-    <script src="../scripts/sections.js"></script>
+    <link rel="stylesheet" href="/styles/font-awesome/all.min.css">
+    <link rel="stylesheet" href="/styles/normalize.css">
+    <link rel="stylesheet" href="/styles/reset.css">
+    <link rel="stylesheet" href="/styles/sections.css">
+    <link rel="stylesheet" href="/styles/general.css">
+    <link rel="stylesheet" href="/styles/news.css">
+    <script src="/scripts/sections.js"></script>
+    <script src="/scripts/news.js"></script>
 </head>
 <body>
 <?php require_once $_SERVER['DOCUMENT_ROOT'] . "/sections/header.php"; ?>
 
 <div class="wrapper">
-    <div class="news">
+    <div class="news" id="news">
         <?php
         StorageRepository::load();
 
@@ -46,16 +47,16 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/util/auth_check.php";
 
                     <div class="news-item__content">
                         <div class="news-item__publishing-date">
-                            <?= $item->getDate()->format('d.m.Y'); ?>
+                            <?= $item->getDate()->format('Y-m-d'); ?>
                         </div>
                         <h1 class="news-item__header"><?= $item->getHeader(); ?></h1>
                         <div class="news-item__keywords">
                             <?php
-                                $keywords = $item->getKeywords();
-                                if (count($keywords) > 0) {
-                                    $keywords = array_map(fn($keyword) => "<b>$keyword</b>", $keywords);
-                                    echo 'Ключові слова: ' . implode(', ', $keywords);
-                                }
+                            $keywords = $item->getKeywords();
+                            if (count($keywords) > 0) {
+                                $keywords = array_map(fn($keyword) => "<b>$keyword</b>", $keywords);
+                                echo 'Ключові слова: ' . implode(', ', $keywords);
+                            }
                             ?>
                         </div>
                         <div class="news-item__text">
@@ -74,11 +75,32 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/util/auth_check.php";
         <?php endforeach; ?>
     </div>
     <div class="news-filter">
-        <form>
-            <label>
-                <input type="text" placeholder="Назва">
-            </label>
-        </form>
+        <h2 class="news-filter__header">Пошук</h2>
+        <label class="news-filter__label" for="news-name-input">
+            Назва шуканої новини:
+        </label>
+        <input id="news-name-input" class="news-filter__input"
+               type="text" placeholder="Назва">
+        <label class="news-filter__label" for="news-date-start">
+            Початок:
+        </label>
+        <input id="news-date-start" class="news-filter__input"
+               type="date">
+        <label class="news-filter__label" for="news-date-end">
+            Кінець:
+        </label>
+        <input id="news-date-end" class="news-filter__input"
+               type="date">
+        <label class="news-filter__label" for="news-keywords">
+            Ключові слова:
+        </label>
+        <div class="news-filter__label">
+            <div id="news-keywords"
+                 class="news-filter__input"
+                 contenteditable="true">
+
+            </div>
+        </div>
     </div>
 </div>
 
