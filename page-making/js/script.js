b297b36e8b7a5188a8ce50b36d09dc0a50988694
ArtemVolsh <77543991+ArtemVolsh@users.noreply.github.com>
2021-05-30 12:44:07 +0300

    Change full screen concept to window
    - Change positions of pagination and slider text
    - Change jQuery class adding function to non-hover

--- page-making/js/script.js
+++ page-making/js/script.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	$('.slider-body').slick({
 		arrows: false,
 		dots: true,
 		pauseOnHover: true,
 		autoplay: true,
-		autoplaySpeed: 10000,
+		autoplaySpeed: 6000,
+		cssEase: 'ease',
 	});
-	
-	$('.slider-item').hover(
-		function(){ $('.slider-text').addClass('non-hover') },
-		function(){ $('.slider-text').removeClass('non-hover') });
 
 	$('.slider-item').hover(
-		function(){ $('.slider-item').addClass('hover') },
-		function(){ $('.slider-item').removeClass('hover') }
-);
+		function () { $('.slider-text').addClass('non-hover') },
+		function () { $('.slider-text').removeClass('non-hover') });
+
+	$('.slider-item').hover(
+		function () { $('.slider-item').addClass('hover') },
+		function () { $('.slider-item').removeClass('hover') }
+	);
 });
