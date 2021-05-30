1bb4e74c026478f6e1f2ef731e7f5c52865828ce
Folkem <maxxfoxx2012@gmail.com>
2021-05-30 17:04:59 +0300

    Add recent page-making changes
    Add changes from commit dd3a851.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,33 +1,41 @@
 <header class="header">
-    <section class="header-body body">
-        <div class="logo-part">
-            <figure class="logo-placeholder">
-                <img class="logo" src="{{ asset('media/static/pgdk_logo.png') }}" width="80px" alt="Логотип ПГФК">
-            </figure>
-            <a href="" class="logo-text">ПГФК</a>
-        </div>
-        <div class="aside-part">
-            <nav class="high-row">
-                <ul class="actions-navigation">
-                    <li class="action-link">
-                        <a href="">Про ресурс</a></li>
-                    <li class="action-link">
-                        <a href="">Події</a></li>
-                    <li class="action-link authorization-link">
-                        <a href="">Авторизація</a></li>
-                </ul>
-            </nav>
-            <nav class="low-row">
-                <ul class="pages-navigation">
-                    <li class="page-link">
-                        <a href="">Розклад</a></li>
-                    <li class="page-link">
-                        <a href="">Студенту</a></li>
-                    <li class="page-link">
-                        <a href="">Новини</a></li>
-                    <!-- <button class="page-link search-button">О</button> -->
-                </ul>
-            </nav>
-        </div>
-    </section>
+    <div class="header-wrapper">
+        <section class="header-body body">
+            <div class="logo-part">
+                <figure class="logo-placeholder">
+                    <img class="logo" src="{{ asset('media/static/pgdk_logo.png') }}" width="80px" alt="Логотип ПГФК">
+                </figure>
+                <a href="" class="logo-text">ПГФК</a>
+            </div>
+            <div class="aside-part">
+                <nav class="high-row">
+                    <ul class="actions-navigation">
+                        <li class="action-link">
+                            <a href="">Про ресурс</a>
+                        </li>
+                        <li class="action-link">
+                            <a href="">Події</a>
+                        </li>
+                        <li class="action-link authorization-link">
+                            <a href="">Авторизація</a>
+                        </li>
+                    </ul>
+                </nav>
+                <nav class="low-row">
+                    <ul class="pages-navigation">
+                        <li class="page-link">
+                            <a href="">Розклад</a>
+                        </li>
+                        <li class="page-link">
+                            <a href="">Студенту</a>
+                        </li>
+                        <li class="page-link">
+                            <a href="">Новини</a>
+                        </li>
+                        <!-- <button class="page-link search-button">О</button> -->
+                    </ul>
+                </nav>
+            </div>
+        </section>
+    </div>
 </header>
--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -9,39 +9,57 @@
 @section('body')
     @include('layouts.header')
 
-    <section class="main-body">
-        <div class="slider body">
-            <div class="slider-body">
-                @for($i = 0; $i < 4; $i++)
-                <div class="slider-item">
-                    <img src="{{ asset('media/static/main-cover.jpg') }}" alt="" class="image-0{{ $i }}">
-                    <div class="slider-text">
-                        <p>В ПГФК, наші студенти <br> мають свободу вибору <br> їхньої спеціальності
-                        </p>
+    <main class="main">
+        <section class="main-body">
+            <div class="slider body">
+                <div class="slider-body">
+                    @for($i = 0; $i < 4; $i++)
+                    <div class="slider-item">
+                        <img src="{{ asset('media/static/main-cover.jpg') }}" alt="" class="image-01">
+                        <div class="slider-text">
+                            <p>В ПГФК, наші студенти <br> мають свободу вибору <br> їхньої спеціальності
+                            </p>
+                        </div>
+                    </div>
+                    @endfor
+                </div>
+            </div>
+            <div class="about body">
+                <span class="outcome">Плоди нашої <br> роботи</span>
+                <hr class="blue-line" width="6.5" size="100">
+                <p>
+                    Зібратися разом - це початок, <br>
+                    Триматися разом - це прогрес, <br>
+                    Працювати разом - це успіх. <br>
+                </p>
+                <button class="pgkd-news">новини пгфк</button>
+            </div>
+        </section>
+        <section class="news">
+            <div class="news-body body">
+                <div class="search-column">
+                    <div class="search-panel">
+                        <div class="search-wrapper">
+                            <input type="text" class="news-search">
+                            <input type="text" class="tag-search">
+                        </div>
                     </div>
                 </div>
-                @endfor
+                <div class="news-column">
+                    @foreach($newsList as $news)
+                        <div class="news-element">
+                            <figure class="news-element-placeholder">
+                                <img src="" alt="" class="news-element-image">
+                            </figure>
+                            <h2 class="news-element-title">{{ $news->title }}</h2>
+                            <p class="news-element-text">{{ \Illuminate\Support\Str::limit($news->body, 200) }}</p>
+                            <p>{{ $news->created_at }}</p>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-        </div>
-        <div class="about body">
-            <span class="outcome">Плоди нашої <br> роботи</span>
-            <hr class="blue-line" width="6.5" size="100">
-            <p>
-                Зібратися разом - це початок, <br>
-                Триматися разом - це прогрес, <br>
-                Працювати разом - це успіх. <br>
-            </p>
-            <button class="pgkd-news">новини пгфк</button>
-        </div>
-    </section>
-
-    @foreach($newsList as $news)
-        <div class="news-element">
-            <figure class="news-element-placeholder"><img src="" alt="" class="news-element-image"></figure>
-            <h2 class="news-element-title">{{ $news->title }}</h2>
-            <p class="news-element-text">{{ $news->body }}</p>
-        </div>
-    @endforeach
+        </section>
+    </main>
 
     @include('layouts.footer')
 @endsection
