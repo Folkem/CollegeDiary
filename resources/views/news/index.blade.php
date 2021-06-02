e835d306038bf20f785160ec6c85391e15cae7b5
ArtemVolsh <77543991+ArtemVolsh@users.noreply.github.com>
2021-06-02 23:59:58 +0300

    Create Notification page
    Add few following icons

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -14,22 +14,24 @@
             <div class="slider body">
                 <div class="slider-body">
                     @for($i = 0; $i < 4; $i++)
-                    <div class="slider-item">
-                        <img src="{{ asset('media/static/main-cover.jpg') }}" alt="" class="image-0{{ $i }}">
-                        <div class="slider-text">
-                            <p>В ПГФК, наші студенти<br>мають свободу вибору<br>їхньої спеціальності</p>
+                        <div class="slider-item">
+                            <img src="{{ asset('media/static/main-cover.jpg') }}" alt="" class="image-0{{ $i }}">
+                            <div class="slider-text">
+                                <p>В ПГФК, наші студенти<br>мають свободу вибору<br>їхньої спеціальності</p>
+                            </div>
                         </div>
-                    </div>
                     @endfor
                 </div>
             </div>
             <div class="about body">
                 <span class="outcome">Плоди нашої <br> роботи</span>
-                <p>
-                    Зібратися разом - це початок,<br>
-                    Триматися разом - це прогрес,<br>
-                    Працювати разом - це успіх.<br>
-                </p>
+                <div class="about-text">
+                    <p>
+                        Зібратися разом - це початок,<br>
+                        Триматися разом - це прогрес,<br>
+                        Працювати разом - це успіх.<br>
+                    </p>
+                </div>
                 <button class="pgkd-news">новини пгфк</button>
             </div>
         </section>
@@ -38,10 +40,12 @@
                 <div class="search-column">
                     <div class="search-panel">
                         <div class="search-wrapper">
-                           <label for=""><input type="text" placeholder="Пошук новини..." class="news-search"></label>
-                           <label for=""><input type="date" placeholder="Рік.Місяць.День" class="datepicker-from"></label>
-                           <label for=""><input type="date" placeholder="Рік.Місяць.День" class="datepicker-to"></label>
-                           <label for=""><input type="text" placeholder="Пошук по тегу..." class="tag-search"></label>
+                            <label for=""><input type="text" placeholder="Пошук новини..." class="news-search"></label>
+                            <label for=""><input type="date" placeholder="Рік.Місяць.День"
+                                                 class="datepicker-from"></label>
+                            <label for=""><input type="date" placeholder="Рік.Місяць.День"
+                                                 class="datepicker-to"></label>
+                            <label for=""><input type="text" placeholder="Пошук по тегу..." class="tag-search"></label>
                         </div>
                     </div>
                 </div>
@@ -52,8 +56,8 @@
                                 <img src="{{ asset('media/static/main-cover.jpg') }}" alt="" class="news-element-image">
                             </figure>
                             <h2 class="news-element-title">
-									 	<a href="{{ route('news.show', $news) }}">{{ $news->title }}</a>
-									 </h2>
+                                <a href="{{ route('news.show', $news) }}">{{ $news->title }}</a>
+                            </h2>
                             <p class="news-element-text">{{ \Illuminate\Support\Str::limit($news->body, 200) }}</p>
                             <p class="news-element-date"> {{ $news->created_at->diffForHumans() }}</p>
                         </div>
