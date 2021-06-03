f091cca8fcc67715f46f86b3c7d38d8eb679f891
Folkem <maxxfoxx2012@gmail.com>
2021-06-03 20:05:36 +0300

    Add previously missing files
    Add common.css
    Add slider.css
    Add slider.js
    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                'bitter-italic': ['"Bither-Italic"', 'sans-serif'],
+                'gotham-pro': ['GothamPro', 'sans-serif'],
+                'gotham-pro-bold': ['"GothamPro-Bold"', 'sans-serif'],
+                'museo-cyrl': ['"Museo Cyrl"', 'sans-serif'],
+            },
+            borderWidth: {
+                DEFAULT: '1px',
+                '0': '0',
+                '2': '2px',
+                '3': '3px',
+                '4': '4px',
+                '5': '5px',
+                '6': '6px',
+                '7': '7px',
+                '8': '8px',
+            },
+            maxWidth: {
+                '1200': '1200px',
+            },
+            height: {
+                fit: 'fit-content',
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("tailwindcss-debug-screens"),
+    ],
+}
