bac31bc85cf10f984d0ac10bbb75654f40bf13ce
ArtemVolsh <77543991+ArtemVolsh@users.noreply.github.com>
2021-06-04 01:12:06 +0300

    Create notification page

---
+++ resources/views/personal/notifications.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('title', 'Сповіщення')
+
+@section('stylesheets')
+
+@endsection
+
+@section('body')
+    @include('layouts.header')
+    <main class="w-full h-full">
+        <div class="container max-w-1200 flex flex-col mx-auto">
+            <div class="head py-10 text-center text-white text-2xl font-gotham-pro-bold bg-blue-600 ">Панель сповіщень
+            </div>
+            <div class="notification-body ">
+                @for($i = 0; $i < 3; $i++)
+                    <div class="notification-item h-16 w-full flex flex-row items-center bg-blue-200 border-solid border-b-2 border-blue-600">
+                        <div class="icon-body pl-10">
+                            <figure class="icon-placeholder w-9 h-9 rounded-full bg-blue-600">
+                                <img src="" alt="" class="icon ">
+                            </figure>
+                        </div>
+                        <div class="title-to-date w-full pr-10 flex justify-between ">
+                            <span class="notification-title pl-5 font-gotham-pro-bold">Вам поставили оцінку з Дисципліни "Web-Design"</span>
+                            <span class="block font-gotham-pro-bold">2021.05.06</span>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+    </main>
+
+    @include('layouts.footer')
+@endsection
+
+@section('scripts')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('news', [NewsController::class, 'index'])->name('news.index');
 Route::get('news/{news}', [NewsController::class, 'show'])->name('news.show');
+Route::view('notifications', 'personal.notifications');
+
 
 Route::redirect('/', 'news');
