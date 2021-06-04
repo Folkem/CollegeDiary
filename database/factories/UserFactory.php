f8e916918dfb039ca8a639eadb7a403eeaa5bca0
Folkem <maxxfoxx2012@gmail.com>
2021-06-04 12:36:57 +0300

    Fix UserFactory default avatar_path
    Previously UserFactory had 'de' for avatar_path
    Change it to 'default-user-avatar.png' which corresponds to the actual
    default user avatar image

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'role_id' => Role::all()->random()->id,
-            'avatar_path' => 'de',
+            'avatar_path' => 'default-user-avatar.png',
         ];
     }
 }
