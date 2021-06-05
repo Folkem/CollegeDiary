a0e0069086117ea8edf46b56b15f022be770ff02
Folkem <maxxfoxx2012@gmail.com>
2021-06-05 19:01:32 +0300

    Add forgoten AuthController
    Previously there was a commit, which added auth logic, but haven't
    had AuthController. Add it.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request): JsonResponse
+    {
+        info($request->input('email'));
+        info($request->json('email'));
+
+        $validated = $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($validated)) {
+            return response()->json([
+                'success' => true,
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => __('auth.failed'),
+        ]);
+    }
+
+    public function logout(): RedirectResponse
+    {
+        auth()->logout();
+
+        return back();
+    }
+}
